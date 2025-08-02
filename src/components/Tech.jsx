d6fc445a13e42d797091230806200b5ee24e6928
Ismaeldevs <PedroIsmaelChavez@alu.frt.utn.edu.ar>
2025-08-02 20:27:31 -0300

    Feature/Nuevas tecnologías

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,6 @@
 import {
     BiLogoBootstrap,
-    BiLogoCss3,
     BiLogoDocker,
-    BiLogoGithub,
     BiLogoHtml5,
     BiLogoJavascript,
     BiLogoMongodb,
@@ -20,10 +18,25 @@ import {
   import { SiDotnet } from "react-icons/si";
   import { GrMysql } from "react-icons/gr";
   import {motion} from "framer-motion"
-  import { RiNotionFill } from "react-icons/ri";
+  import BiLogoNext from "../icons/BiLogoNext";
+  import CSSNew from "../icons/CSSNew";
+  import Supabase from "../icons/Supabase";
+  import Bash from "../icons/Bash";
+  import Python from "../icons/Python";
+  import Kali from "../icons/Kali";
 
   
   import Reveal from "./Reveal";
+import Linux from "../icons/Linux";
+import Cloudflare from "../icons/Cloudflare";
+import GitHub from "../icons/GitHub";
+import Vercel from "../icons/Vercel";
+import Ngrok from "../icons/Ngrok";
+import MicrosoftSQLServer from "../icons/MicrosoftSQLServer";
+import PostgreSQL from "../icons/PostgreSQL";
+import Railway from "../icons/Railway";
+import FastAPI from "../icons/FastAPI";
+import Nodejs from "../icons/Nodejs";
   
   
   const Tech = () => {
@@ -35,33 +48,52 @@ import {
         category: "Frontend",
         technologies: [
           { name: "HTML", icon: <BiLogoHtml5 className="text-orange-600" /> },
-          { name: "CSS", icon: <BiLogoCss3 className="text-blue-600" /> },
+          { name: "CSS", icon: <CSSNew className="text-blue-600" /> },
           { name: "Bootstrap", icon: <BiLogoBootstrap className="text-purple-600" /> },
           { name: "Tailwind CSS", icon: <BiLogoTailwindCss className="text-teal-500" /> },
           { name: "JavaScript", icon: <BiLogoJavascript className="text-yellow-500" /> },
           { name: "TypeScript", icon: <BiLogoTypescript className="text-blue-500" /> },
           { name: "React", icon: <BiLogoReact className="text-cyan-500" /> },
+          { name: "Next.js", icon: <BiLogoNext /> },
         ],
       },
       {
-        category: "Backend",
+        category: "Backend y Bases de Datos",
         technologies: [
           { name: "MongoDB", icon: <BiLogoMongodb className="text-green-600" /> },
-          { name: "PostgreSQL", icon: <BiLogoPostgresql className="text-blue-700" /> },
+          { name: "PostgreSQL", icon: <PostgreSQL /> },
           { name: "MySQL", icon: <GrMysql className="text-cyan-600" /> },
-          { name: "ExpressJs", icon: <SiExpress className="text-gray-600" /> },
+          { name: "Microsoft SQL Server", icon: <MicrosoftSQLServer />},
+          { name: "Supabase", icon: <Supabase /> },
+          { name: "NodeJS", icon: <Nodejs /> },
+          { name: "ExpressJS", icon: <SiExpress className="text-gray-600" /> },
           { name: "NestJS", icon: <BiLogoNest /> },
+          { name: "FastAPI", icon: <FastAPI /> },
           { name: "Dotnet", icon: <SiDotnet className="text-blue-600" /> },
         ],
       },
       {
-        category: t("tech.category"),
+        category: t("tech.categoryCS"),
+        technologies: [
+          { name: "Bash", icon: <Bash /> },
+          { name: "Python", icon: <Python />},
+          { name: "Kali Linux", icon: <Kali /> },
+          { name: "Linux", icon: <Linux /> },
+          { name: "Ngrok", icon: <Ngrok /> },
+
+        ],
+      },
+        {
+        category: t("tech.categoryInfra"),
         technologies: [
           { name: "Jira", icon: <SiJira className="text-blue-600" /> },
           { name: "Notion", icon: <BiLogoNotion  className="text-black" /> },
-          { name: "Docker", icon: <BiLogoDocker className="text-blue-500" /> },
           { name: "Git", icon: <BiLogoGit className="text-orange-600" /> },
-          { name: "GitHub", icon: <BiLogoGithub className="text-gray-600" /> },
+          { name: "GitHub", icon: <GitHub className="text-gray-600" /> },
+          { name: "Docker", icon: <BiLogoDocker className="text-blue-500" /> },
+          { name: "Cloudflare", icon: <Cloudflare /> },
+          { name: "Vercel", icon: <Vercel /> },
+          { name: "Railway", icon: <Railway /> },
         ],
       },
     ];
